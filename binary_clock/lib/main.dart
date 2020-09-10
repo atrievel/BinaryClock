6fc3c66bdecfbd69f298a7e202981080f5441c15
Alec Trievel <atrievel@ceiamerica.com>
2020-09-10 13:20:00 -0400

    Force app binding before changing orientation

--- binary_clock/lib/main.dart
+++ binary_clock/lib/main.dart
@@ -4,7 +4,10 @@ import 'package:flutter/services.dart' show SystemChrome, DeviceOrientation;
 import './clock.dart';
 
 void main() {
-  // Start the app in landscape mode to fit the clock on the screen
+  // force the app to bind first before changing the orientation
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // start the app in landscape mode to fit the clock on the screen
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight]);
 
@@ -16,10 +19,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-          fontFamily: 'Alatsi',
-          brightness: Brightness.dark,
-          textTheme: TextTheme(
-              headline4: TextStyle(color: Colors.black38, fontSize: 30))),
+        fontFamily: 'Alatsi',
+        brightness: Brightness.dark,
+        textTheme: TextTheme(
+          headline4: TextStyle(color: Colors.black38, fontSize: 30),
+        ),
+      ),
       home: Scaffold(
         body: Clock(),
       ),
